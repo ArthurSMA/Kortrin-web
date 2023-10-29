f5eb0a03f34c429a38a1353f6c88dc1082851023
ArthurSMA <arthursuassunamaiaalves@gmail.com>
2023-10-29 16:48:34 -0300

    master: modificacoes feitas são lista, remover, notificacao

---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from "react";
+import Usuario from "./Usuario";
+
+const URL_API = "http://localhost:9090/api/usuarios";
+
+export default function Home() {
+    return <Usuario />;
+}
